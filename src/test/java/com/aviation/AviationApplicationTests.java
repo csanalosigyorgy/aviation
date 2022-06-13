347ee8dcb98c198a6a14d997dbb5be2d9b4da1a7
György Csanálosi <csanalosigyorgy@gmail.com>
2022-06-13 17:08:43 +0200

    Initial commit

---
+++ src/test/java/com/aviation/AviationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aviation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
